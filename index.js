ba889afac35e21670efa58681f4629a6471c2bd2
JiSop <jisop.shin@gmail.com>
2020-08-07 11:08:13 +0900

    Add resolvers, schema

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga';
-
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-  },
-};
+import resolvers from './graphql/resolvers';
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: 'graphql/schema.graphql',
   resolvers
 });
 
